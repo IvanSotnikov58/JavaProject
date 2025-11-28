f888e88ff82556f7da0c143ac90fdb90f2623149
yungnik911 <nikita.kirejev@ivkhk.ee>
2025-11-28 12:33:49 +0200

    izmenil CardService

--- src/main/java/com/example/hearthclone/Service/CardService.java
+++ src/main/java/com/example/hearthclone/Service/CardService.java
@@ -16,27 +16,27 @@ public class CardService {
         this.cardRepository = cardRepository;
     }
 
-    // Получить все карты
+
     public List<Cards> getAllCards() {
         return cardRepository.findAll();
     }
 
-    // Добавить карту
+
     public Cards addCard(Cards card) {
         return cardRepository.save(card);
     }
 
-    // Удалить карту по id
+
     public void deleteCard(Long id) {
         cardRepository.deleteById(id);
     }
 
-    // Найти карту по имени
+
     public List<Cards> findByName(String name) {
         return cardRepository.findByName(name);
     }
 
-    // Найти карты по типу
+
     public List<Cards> findByType(String type) {
         return cardRepository.findByType(type);
     }
