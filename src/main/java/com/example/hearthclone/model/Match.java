63cdc2db540d755bc9b2a81ce5d45874de491bed
yungnik911 <nikita.kirejev@ivkhk.ee>
2025-11-28 18:45:36 +0200

    Match izmenil

--- src/main/java/com/example/hearthclone/model/Match.java
+++ src/main/java/com/example/hearthclone/model/Match.java
@@ -18,13 +18,21 @@ public class Match {
     private User player02;
 
     // <- changed to wrapper Long
-    private Long winner;
+    private User winner;
 
     private String status;
+    @OneToOne
+    private Decks decks1;
+    @OneToOne
+    private Decks decks2;
+    private int player01Health = 100;
+    private int player02Health = 100;
 
-    protected Match() {}
 
-    public Match(User player01, User player02, Long winner, String status) {
+    protected Match() {
+    }
+
+    public Match(User player01, User player02, User winner, String status) {
         this.player01 = player01;
         this.player02 = player02;
         this.winner = winner;
@@ -58,8 +66,13 @@ public class Match {
         this.player02 = player02;
     }
 
-    public Long getWinner() { return winner; }
-    public void setWinner(Long winner) { this.winner = winner; }
+    public Long getWinner() {
+        return winner.getId();
+    }
+
+    public void setWinner(User winner) {
+        this.winner = winner;
+    }
 
     public String getStatus() {
         return status;
@@ -70,8 +83,37 @@ public class Match {
     }
 
     public void setDeck1(Decks decks1) {
+        this.decks1 = decks1;
     }
 
     public void setDeck2(Decks decks2) {
+        this.decks2 = decks2;
+    }
+
+    public Decks getDecks1() {
+        return decks1;
+    }
+
+    public Decks getDecks2() {
+        return decks2;
+    }
+
+    public int getPlayerHealth(User player) {
+        if (player.getId().equals(player01.getId())) return player01Health;
+        else return player02Health;
+    }
+
+    public void setPlayerHealth(User player, int health) {
+        if (player.getId().equals(player01.getId())) player01Health = health;
+        else player02Health = health;
+    }
+
+    public int getPlayer01Health() {
+        return player01Health;
+    }
+    public int getPlayer02Health(){
+        return player02Health;
     }
 }
+
+
