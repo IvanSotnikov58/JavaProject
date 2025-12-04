f13994b1d6448b6f5ac360376f3f14d459e7298e
yungnik911 <nikita.kirejev@ivkhk.ee>
2025-12-04 22:20:06 +0200

    izmenil TurnRepository

--- src/main/java/com/example/hearthclone/Repository/TurnRepository.java
+++ src/main/java/com/example/hearthclone/Repository/TurnRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TurnRepository extends JpaRepository<Turn, Long> {
-    List<Turn> findByMatchIdOrderByTimestampAsc(Long matchId);
+
+    List<Turn> findByMatch_IdOrderByTimestampAsc(Long matchId);
 
 }
